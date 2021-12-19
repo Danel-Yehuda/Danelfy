3807b0ad808eddd4ad49414d966c7952d6e15d80
MichaelMishin <michaelmishin132@gmail.com>
2021-12-19 11:52:27 -0500

    added server connection url

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true})) // req.body
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors())
+
+const CONNECTION_URL = "mongodb+srv://danel:danel@cluster0.ek9pq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" //the url to connect the database
+const PORT = process.env.PORT || 5000 //the port the server working on
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+.catch((error) => console.log(error))
